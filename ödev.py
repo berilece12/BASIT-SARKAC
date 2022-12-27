f616826ef46704aea86229dbbf156c26dfeedff2
Duygu Karasakal <117751865+dkduygu@users.noreply.github.com>
2022-12-27 22:03:01 +0300

    Update ödev.py

--- ödev.py
+++ ödev.py
@@ -23,7 +23,9 @@ BACKGROUND = pygame.image.load(os.path.join("image", "background.jpg"))
 BACKGROUND_IMAGE = pygame.transform.scale(
   BACKGROUND, (HEIGHT, HEIGHT))
 BACKGROUND_IMAGE.set_alpha(50)
-BALL_START_X, BALL_START_Y = HEIGHT / 2, ROPE_LENGTH_START + 20
+
+top_için_başlangiç_x, top_için_başlangiç_y = uzunluk / 2, ipin_başlangiç_uzunluğu + 20
+ 
 
 değer_çubuğu_uzunluğu = 250 
 
